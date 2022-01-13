32e4524b29344a2e832b4a02b2e105762f55b4ed
Diana Baiadori <diana.baiadori@gmail.com>
2022-01-13 12:35:24 +0100

    feature: worked on the logic of clicking on a square to put down a mark

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,10 +1,41 @@
 class ChoicesController < ApplicationController
   def index
     @choices = Choice.all
+    @choice = Choice.new
+    @win = win
   end
 
-  def create
-    Choice.create(choice_params)
-    render new
+  def update
+    Choice.update(choice_params)
+    redirect_to root_path
+  end
+
+  def win
+    lines = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+    victory = false
+    lines.each do |line|
+      line_through = []
+      line.each do |position|
+
+      end
+
+    end
+    # column = [[1,4,7],
+    #           [2,5,8],
+    #           [3,6,9]]
+    # diagonal = [[1,5,9]]
+    if 1 == 2
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def choice_params
+    params.require(:choice).permit(:position, :mark)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'choices#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :choices, only: %i[index create]
+  resource :choices, only: %i[index update]
 end
