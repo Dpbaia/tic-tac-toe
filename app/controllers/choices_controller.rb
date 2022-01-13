4f87b249414de35d54f11b32bd81c98c58c6813d
Diana Baiadori <diana.baiadori@gmail.com>
2022-01-13 12:57:28 +0100

    feature: added method for determining if game is finished

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -11,26 +11,30 @@ class ChoicesController < ApplicationController
   end
 
   def win
-    lines = [[1,2,3],
-            [4,5,6],
-            [7,8,9]]
+    lines = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+    columns = [[1, 4, 7],
+               [2, 5, 8],
+               [3, 6, 9]]
+    diagonals = [[1, 5, 9],
+                 [3, 5, 7]]
+
+    lines_check(lines) || lines_check(columns) || lines_check(diagonals)
+
+  end
+
+  def lines_check(lines)
     victory = false
     lines.each do |line|
       line_through = []
       line.each do |position|
-
+        positions_mark = Choice.all.find_by(position: position).mark
+        line_through.append(positions_mark) unless line_through.include?(positions_mark)
       end
-
+      victory = true if line_through.length == 1 && line_through[0] != 'none'
     end
-    # column = [[1,4,7],
-    #           [2,5,8],
-    #           [3,6,9]]
-    # diagonal = [[1,5,9]]
-    if 1 == 2
-      true
-    else
-      false
-    end
+    victory
   end
 
   private
